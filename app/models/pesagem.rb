021335da3b32415943a06293f35b23b19e5696cd
Anderson <andersonodilo@hotmail.com>
2014-08-05 19:52:53 -0300

    scope animal em pesagens

--- app/models/pesagem.rb
+++ app/models/pesagem.rb
@@ -1,5 +1,6 @@
 class Pesagem < ActiveRecord::Base
   belongs_to :animal
+  scope :animal, ->(animal) {where(:animal_id => animal)}
   scope :propriedade, ->(usuario) {Pesagem.joins(:animal).where("animais.propriedade_id = ?", usuario.propriedade_id)}
   validates :animal, presence: true
   validates :peso, presence: true
