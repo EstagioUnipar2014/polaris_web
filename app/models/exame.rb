6839446f4dca2325a9d8beace2fdbe311360cdde
Jose Luiz Tafarel <jltafarel@gmail.com>
2014-08-04 22:31:26 -0300

    testes do modelo exame #1

--- app/models/exame.rb
+++ app/models/exame.rb
@@ -2,8 +2,10 @@ class Exame < ActiveRecord::Base
   belongs_to :animal
   belongs_to :tipo_exame
   has_and_belongs_to_many :doencas, dependent: :destroy
-  
-  validates :animal_id, :tipo_exame_id, presence: true
-  
+
+  validates :animal, presence: true
+  validates :tipo_exame, presence: true
+  validates :data, presence: true
+
   scope :pendente, -> {where(efetuado: false)}
 end
