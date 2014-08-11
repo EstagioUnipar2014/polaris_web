1fa2cef4186ebde9ef83622973d29d224b7d3b37
Jose Luiz Tafarel <jltafarel@gmail.com>
2014-08-11 19:21:13 -0300

    modificando estrutura dos cios e coberturas

--- app/models/cio.rb
+++ app/models/cio.rb
@@ -1,5 +1,7 @@
 class Cio < ActiveRecord::Base
   belongs_to :animal
-  
+  has_one :cobertura
+
+  accepts_nested_attributes_for :cobertura
   scope :propriedade, ->(usuario) {Cio.joins(:animal).where("animais.propriedade_id = ?", usuario.propriedade_id)}
 end
