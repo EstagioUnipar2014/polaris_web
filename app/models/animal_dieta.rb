261205351fe3d552a778c3f85ad2b54bc885d66e
Anderson <andersonodilo@hotmail.com>
2014-08-28 21:58:24 -0300

    scope dieta ativa

--- app/models/animal_dieta.rb
+++ app/models/animal_dieta.rb
@@ -2,5 +2,4 @@ class AnimalDieta < ActiveRecord::Base
   belongs_to :dieta
   belongs_to :animal
 
-  scope :ativa, -> {where :data_fim.empty?}
 end
--- app/models/dieta.rb
+++ app/models/dieta.rb
@@ -3,4 +3,6 @@ class Dieta < ActiveRecord::Base
   has_many :animais, :through => :animal_dieta
   has_and_belongs_to_many :ciclos, :allow_destroy => true
   accepts_nested_attributes_for :ciclos
+
+  scope :ativa, -> {Dieta.joins(:animal_dieta).where("animal_dieta.data_fim IS NULL")}
 end
