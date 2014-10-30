f87f4815afc83c8c4acaee61e189bfc3f6249311
Anderson <andersonodilo@hotmail.com>
2014-10-30 18:38:33 -0200

    helper true false

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def t_f(boolean)
+      if boolean == true
+        image_tag("icon-true.png")
+      elsif boolean == false
+        image_tag("icon-false.png")  
+      end
+  end
 end
