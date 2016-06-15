ef80c63b7ae211cec3729fbc8c99f29b8b1c16aa
José Luiz Tafarel <jltafarel@gmail.com>
2016-06-15 18:52:49 -0300

    Atualiza gems do projeto e corrige erro do feed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,6 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_usuario!
   def index
     if usuario_signed_in?
-      @feed = Feedjira::Feed.fetch_and_parse('http://www.ruralbr.com.br/ultimas-noticias-rss/')
       render 'index'
     else
       render 'index_unlogged'
